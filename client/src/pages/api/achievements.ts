30c93149be802f0d637a812aab0f9e2050793822
dnm10 <deeptinm10@gmail.com>
2025-10-30 22:48:15 +0530

    Api's for Chatbot

---
+++ client/src/pages/api/achievements.ts
@@ -0,0 +1,7 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { storage } from 'server/storage';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const portfolio = await storage.getPortfolio();
+  res.status(200).json(portfolio.achievements);
+}
